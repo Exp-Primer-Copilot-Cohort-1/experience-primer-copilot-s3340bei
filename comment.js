38e357f03dabe219738f0f466109f4de362c3948
bei <93430194+s3340bei@users.noreply.github.com>
2023-12-06 11:47:17 +0000

    Copilot third commit

--- comment.js
+++ comment.js
@@ -1,20 +1,33 @@
 // Create web server
-const express = require("express");
-const router = express.Router();
-const commentController = require("../controllers/commentController");
-const { check } = require("express-validator");
+// 1. Create a web server
+// 2. Read the file
+// 3. Send the file to the client
+// 4. End the response
 
-// api/comment
-router.post(
-  "/",
-  [
-    check("comment", "Comment is required").not().isEmpty(),
-    check("project", "Project is required").not().isEmpty(),
-  ],
-  commentController.createComment
-);
-router.get("/:project", commentController.getComments);
-router.put("/:id", commentController.updateComment);
-router.delete("/:id", commentController.deleteComment);
+// 1. Create a web server
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+    // 2. Read the file
+    // const data = fs.readFileSync(`${__dirname}/userApi/userApi.json`, 'utf-8');
+    // const objData = JSON.parse(data);
+    // console.log(objData);
+    // 3. Send the file to the client
+    // res.end(objData[0].name);
+    // 4. End the response
+    // res.end('Hello from the other side');
+    // console.log(req.url);
+    if (req.url == '/') {
+        res.end('Hello from the other side');
+    } else if (req.url == '/about') {
+        res.end('Hello from the about side');
+    } else if (req.url == '/contact') {
+        res.end('Hello from the contact side');
+    } else {
+        res.writeHead(404, { 'Content-type': 'text/html' });
+        res.end('<h1>404 error page. Page does not exist</h1>');
+    }
+});
 
-module.exports = router;
+// 2. Listen to the server
+server.listen(8000, '');
